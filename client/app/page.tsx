08e8ddb7fed1247776b0005ad7fc7e29a1c8a256
Hegyi Krisztián <hegyikrisztian1@gmail.com>
2026-08-07 20:21:49 +0200

    Remove instabil font, fix width on forms.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,8 @@
-import { Bitcount_Grid_Double } from "next/font/google";
 import Link from "next/link";
 import { mainButtonClassName, secondaryButtonClassName } from "./ui/buttons/button";
 import { verifySession } from "./lib/session";
 import { ViewTransition } from "react";
 
-const bitcountGrid = Bitcount_Grid_Double({
-  subsets: ["latin"],
-});
 
 const LandingPage = async () => {
     const { isAuthenticated } = await verifySession();
@@ -15,7 +11,7 @@ const LandingPage = async () => {
         <ViewTransition enter='slide-up' exit='slide-down'>
             <main className="flex flex-col justify-center items-center bg-gray-900 p-0 m-auto h-full w-full">
                 <div className="flex flex-col justify-center w-[50%] items-start gap-2.5 bg-gray-900 p-0 m-auto h-full">
-                    <h1 className={`${bitcountGrid.className} leading-18.75 m-0 text-9xl uppercase`}>
+                    <h1 className={`leading-18.75 m-0 text-9xl uppercase`}>
                         Notes
                     </h1>
                     <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis tenetur ducimus consequuntur sit at qui doloremque earum quas animi illo fugiat corporis dolores maxime, ipsa expedita amet magnam doloribus velit!</article>
